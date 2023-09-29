0c6122207ac3511900a805afb16d37fe537887e7
Joseph Hydon <j.d.hydon@gmail.com>
2023-09-29 20:43:56 +0100

    add test image

--- main.cpp
+++ main.cpp
@@ -19,8 +19,13 @@
 
 //#define PLATFORM_WEB
 
+
+
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
+#define ASSETPATH "resources/"
+#else
+#define ASSETPATH "../resources/"
 #endif
 
 //----------------------------------------------------------------------------------
@@ -32,7 +37,9 @@ const int screenHeight = 533;
 //----------------------------------------------------------------------------------
 // Module functions declaration
 //----------------------------------------------------------------------------------
-void UpdateDrawFrame(void);     // Update and Draw one frame
+void UpdateDrawFrame(void); // Update and Draw one frame
+
+Texture2D monster;
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -42,10 +49,15 @@ int main(void)
     // Initialization
     //--------------------------------------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    monster = LoadTexture(ASSETPATH "testasset.png");
+
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
+
+
+
     SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -81,6 +93,7 @@ void UpdateDrawFrame(void)
     ClearBackground(RAYWHITE);
 
     DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+    DrawTexturePro(monster, {0, 0, (float)monster.width, (float)monster.height}, {screenWidth / 2.0f, screenHeight / 2.0f, (float)monster.width, (float)monster.height }, {monster.width/2.0f, monster.height/2.0f}, 0.0f, WHITE);
 
     EndDrawing();
     //----------------------------------------------------------------------------------
